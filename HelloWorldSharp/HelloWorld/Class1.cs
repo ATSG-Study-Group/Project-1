b6fcfc0d45392f141942dcca24e4921390176c79
David Watchorn <dave@thewatchorns.org>
2026-07-10 17:43:41 -0400

    Hello World

---
+++ HelloWorldSharp/HelloWorld/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Crestron.SimplSharp;
+
+namespace HelloWorld
+{
+    public class Class1
+    {
+        public void SayHello()
+        {
+            CrestronConsole.PrintLine("Hello, World!");
+        }
+    }
+}
