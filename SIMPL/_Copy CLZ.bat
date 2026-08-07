869e9d62ce489905df9e1fd5635e85927d002ad6
David Watchorn <dave@thewatchorns.org>
2026-08-07 17:51:33 -0400

    Library day 1
    Roughed in SIMPL, SIMPL+, VTPro and VS Solution
    
    SGP1-23
    SGP1-20
    SGP1-21
    SGP1-24
    SGP1-22

---
+++ SIMPL/_Copy CLZ.bat
@@ -0,0 +1,2 @@
+copy "C:\Users\dave\Development\WorkProjects\ATSG StudyGroup\Project-1\Sharp\LibrarySystem\bin\Debug\LibrarySystem.clz" "C:\Users\dave\Development\WorkProjects\ATSG StudyGroup\Project-1\SIMPL"
+#pause
---
+++ Sharp/LibrarySystem/SIMPLLibraryUI.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Crestron.SimplSharp;
+using CrestronSharp;
+
+namespace LibrarySystem
+{
+    // Class to function as our interface between C# and SIMPL+
+    public class SIMPLLibraryUI
+    {
+        // the default constructor for the class -- SIMPL+ can only call the default constructor
+        public SIMPLLibraryUI() 
+        { 
+        }
+
+        // initialization logic goes here
+        public ushort Init()
+        {
+            return 1;
+        }
+
+    }
+}
